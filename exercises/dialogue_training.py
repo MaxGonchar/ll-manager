6757f477c4d5123a67623bbd98c4cf558cee8fce
MaxGonchar <boborodada@gmail.com>
2025-03-04 19:54:22 +0200

    implement new dialogue route

---
+++ exercises/dialogue_training.py
@@ -0,0 +1,15 @@
+class DialogueTraining:
+    def __init__(self, user_id: str):
+        self.user_id = user_id
+    
+    def create_dialogue(self, title: str, description: str) -> str:
+        pass
+
+    def get_dialogue(self, dialogue_id: str):
+        pass
+
+    def get_dialogues(self):
+        pass
+
+    def submit_dialogue_statement(self, dialogue_id: str, statement: str):
+        pass
--- routes/dialogue_training.py
+++ routes/dialogue_training.py
@@ -1,10 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, g
+
+from forms.dialogue_train_forms import DialogueCreateForm
+from helpers.rbac_helper import role_required
+from constants import Role
+from exercises.dialogue_training import DialogueTraining
 
 dialogue_training_bp = Blueprint("dialogue_training", __name__, url_prefix="/exercise")
 
 
 @dialogue_training_bp.route("/dialogues", methods=["GET"])
 def dialogues():
+    role_required(
+        [
+            Role.SUPER_ADMIN.value,
+            Role.ADMIN.value,
+            Role.SELF_EDUCATED.value,
+        ]
+    )
     dialogues = [
         {"id": "123", "title": "Dialogue 1 qweqweqweqweqweqweqweqwe", "description": "This is a description"},
         {"id": "123", "title": "Dialogue 2 qweqweqweqweqweqweqweqwe", "description": "This is a description"},
@@ -18,11 +30,23 @@ def dialogues():
 
 @dialogue_training_bp.route("/dialogues/new", methods=["GET", "POST"])
 def dialogue_new():
-    # ask dialogue name
-    # ask dialogue description
-    # create dialogue
-    # redirect to dialogue
-    pass
+    role_required(
+        [
+            Role.SUPER_ADMIN.value,
+            Role.ADMIN.value,
+            Role.SELF_EDUCATED.value,
+        ]
+    )
+    form = DialogueCreateForm()
+
+    if form.validate_on_submit():
+        dialogue_id = DialogueTraining(g.user_id).create_dialogue(
+            title=form.title.data,
+            description=form.description
+        )
+        return render_template("PAGE FOR DIALOGUE")
+
+    return render_template("exercises/dialogue_training_new.html", form=form)
 
 
 @dialogue_training_bp.route("/dialogues/<dialogue_id>", methods=["GET", "POST"])
